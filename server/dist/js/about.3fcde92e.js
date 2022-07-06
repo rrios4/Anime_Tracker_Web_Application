ce8f7273227ca8e87be9f90ab9e1276942193229
Rogelio Rios <rogeliorios348@gmail.com>
2022-07-05 22:39:02 -0500

    Added dist folder to node server folder

---
+++ server/dist/js/about.3fcde92e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,a,c){"use strict";c.r(a);var e=c("7a23"),n={class:"about"},s=Object(e["g"])("h1",{class:"text-black"},"This is an about page",-1);function o(t,a){return Object(e["p"])(),Object(e["d"])("div",n,[s])}const b={};b.render=o;a["default"]=b}}]);
+//# sourceMappingURL=about.3fcde92e.js.map
